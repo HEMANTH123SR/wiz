fe821fb895a34ccf5f50690cc2440a9725557d87
HEMANTH123SR <applicationcomputer257@gmail.com>
2024-03-26 12:24:39 +0530

    2

--- app/page.tsx
+++ app/page.tsx
@@ -55,6 +55,25 @@ export default function Home() {
         <h1 className="text-6xl text-center text-white">
           Hogwarts Welcome you
         </h1>
+        <p className="text-center text-white py-12 px-6">Hogwarts is one of the world’s finest wizarding schools, located in a massive castle in Scotland. Hogwarts takes students from all over Great Britain, starting at the age of 11. The students leave school after their seventh year.
+
+          The students at Hogwarts are sorted at the beginning of their first year into one of four houses: Gryffindor, Hufflepuff, Ravenclaw, and Slytherin. The houses are named after the four founders of the school, who established Hogwarts around 990</p>
+        <p className="text-center text-white px-6">Hogwarts is home to a student body of between 300 and 400 students, around fifteen teachers and staff, numerous ghosts, house-elves, and a particularly annoying poltergeist. The many rooms, corridors, staircases, passageways, and dungeons hold many secrets and mysteries from a thousand years’ worth of magical history.
+
+          The school motto, which appears on the crest, is “Draco dormiens nunquam titillandus,” which means “Never tickle a sleeping dragon.”</p>
+      </div>
+
+      <div className="flex flex-col justify-center items-center space-y-12 mt-16">
+        <h1 className="text-6xl text-center text-white">Hogwarts Houses</h1>
+        <div className="flex justify-center space-x-28 items-center">
+          <img src="Houses/Griffindor.png" className="w-32 h-auto" />
+          <img src="Houses/Huffelpuff.png" className="w-32 h-auto" />
+          <img src="Houses/MafloyHouseEWW.png" className="w-32 h-auto" />
+          <img src="Houses/RavenClaw.png" className="w-32 h-auto" />
+        </div>
+      </div>
+      <div className="h-44">
+
       </div>
     </main>
   );
