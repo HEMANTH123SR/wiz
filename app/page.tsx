f78830628712372cbeec77bb52678491be1b6417
HEMANTH123SR <applicationcomputer257@gmail.com>
2024-03-26 13:01:21 +0530

    no message

--- app/page.tsx
+++ app/page.tsx
@@ -11,18 +11,10 @@ export default function Home() {
       <nav className="flex flex-row h-20 w-full justify-between items-center px-4">
         <img src="NavBarLogo.png" className="w-12 h-auto" />
         <div className="flex justify-center items-center space-x-10  bg-[#B5935B] h-full px-24 rounded-b-2xl">
-          <span className="text-3xl font-semibold text-white">
-            Home
-          </span>
-          <span className="text-3xl font-semibold  text-white">
-            About Us
-          </span>
-          <span className="text-3xl font-semibold  text-white">
-            House
-          </span>
-          <span className="text-3xl font-semibold  text-white">
-            Course
-          </span>
+          <span className="text-3xl font-semibold text-white">Home</span>
+          <span className="text-3xl font-semibold  text-white">About Us</span>
+          <span className="text-3xl font-semibold  text-white">House</span>
+          <span className="text-3xl font-semibold  text-white">Course</span>
         </div>
         <div className="flex justify-center items-center px-3.5 py-2 bg-[#B5935B] rounded-md  text-xl text-white">
           Attend
@@ -48,9 +40,9 @@ export default function Home() {
         style={{
           backgroundImage: "url(HogwartsDining.jpg)",
           backgroundPosition: "center",
-          height: "500px",
+          height: "700px",
           width: "auto",
-          border: '2px solid #B5935B'
+          border: "2px solid #B5935B",
         }}
       >
         <h1 className="text-6xl text-center text-white">
@@ -85,25 +77,53 @@ export default function Home() {
         </div>
       </div>
 
-
       <div className="h-44"></div>
       <div className="flex flex-col space-y-7 mx-12">
-        <div className="flex space-x-6 bg-[#B5935B] rounded-lg justify-around items-center py-16">
+        <div
+          className="flex space-x-6  rounded-lg justify-around items-center py-16"
+          style={{
+            backgroundImage: "url(CoursesBg.jpg)",
+            backgroundPosition: "center",
+            backgroundSize: "clip",
+          }}
+        >
           <h1 className="text-6xl font-semibold text-white">First Year</h1>
-          <img src="Subjects/transfiguration.png" className="h-44 w-44 rounded-2xl" />
-          <img src="Subjects/alchemy.png" className="h-44 w-44 rounded-2xl" />
-          <img src="Subjects/dd.png" className="h-44 w-44 rounded-2xl" />
+          <img
+            src="Subjects/transfiguration.png"
+            className="h-44 w-44 rounded-2xl border-2 border-black"
+          />
+          <img
+            src="Subjects/alchemy.png"
+            className="h-44 w-44 rounded-2xl border-2 border-black"
+          />
+          <img
+            src="Subjects/dd.png"
+            className="h-44 w-44 rounded-2xl border-2 border-black"
+          />
         </div>
-        <div className="flex space-x-6 bg-[#B5935B] rounded-lg justify-around items-center py-16">
+        <div className="flex space-x-6  rounded-lg justify-around items-center py-16"
+          style={{
+            backgroundImage: "url(CoursesBg.jpg)",
+            backgroundPosition: "center",
+            backgroundSize: "clip",
+          }}
+        >
           <h1 className="text-6xl font-semibold text-white">Second Year</h1>
-          <img src="Subjects/transfiguration.png" className="h-44 w-44 rounded-2xl" />
-          <img src="Subjects/alchemy.png" className="h-44 w-44 rounded-2xl" />
-          <img src="Subjects/dd.png" className="h-44 w-44 rounded-2xl" />
+          <img
+            src="Subjects/cosmology.png"
+            className="h-44 w-44 rounded-2xl border-2 border-black"
+          />
+          <img
+            src="Subjects/hebology.png"
+            className="h-44 w-44 rounded-2xl border-2 border-black"
+          />
+          <img
+            src="Subjects/Potions.png"
+            className="h-44 w-44 rounded-2xl border-2 border-black"
+          />
         </div>
-
       </div>
-      <div className="h-52">
-      </div>
+      <div className="h-52"></div>
     </main>
   );
 }
